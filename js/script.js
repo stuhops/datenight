1791413ca67b8afb0220ab6478848505794b7c84
Kohl  Kennedy <kohl.kennedy@aggiemail.usu.edu>
2019-11-16 10:26:27 -0700

    Updated Script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+<<<<<<< Updated upstream
 api_key=AIzaSyAMs2k4rr3VCdl2AV7CCozKLbNOmJtNsLc
+=======
+document.addEventListener('DOMContentLoaded', function () {
+    /* your logic here */
+    document.body.style.backgroundColor = "grey";
+ });
+
+
+
+
+function api_call(){
+    let url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=bar&keyword=cruise&key=AIzaSyCWafdsAvOrGNrtB7RPOmjcfwt5aNkJEvw"
+    fetch(url) // make a request of the URL
+    .then( r => r.json() )  // convert the response into JSON
+    .then( json => {
+        myJSONResult = json;
+        //document.querySelector('#quandl').textContent = "Your data is available in the variable 'theData'";
+        //document.title = theData.dataset.name;
+        for (i = 0; i < myJSONResult.results.length; i++) {
+            myAddress[i] = myJSONResult.results[i].formatted_address;
+        }
+    });
+}
+
+>>>>>>> Stashed changes
