e78d881c6db1e23b4e80cb93774a5bab6b56c53b
stuhops <stuart.hopkins@aggiemail.usu.edu>
2019-11-16 14:16:47 -0700

    Added a submit button

--- js/searchForm.js
+++ js/searchForm.js
@@ -12,10 +12,9 @@ function showTab(n) {
     document.getElementById("prevBtn").style.display = "inline";
   }
   if (n == (x.length - 1)) {
-    document.getElementById("nextBtn").innerHTML = "Submit";
-    document.getElementById("nextBtn").onclick = "handleSubmit()";
+    document.getElementById("nextBtn").display = "none";
   } else {
-    document.getElementById("nextBtn").innerHTML = "Next";
+    document.getElementById("nextBtn").display = "inline";
   }
   // ... and run a function that displays the correct step indicator:
   fixStepIndicator(n)
