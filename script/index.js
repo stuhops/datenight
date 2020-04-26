ad9d1e41e57d1fe168eb92033dd16c2af59ee185
stuhops <stuart.hopkins@aggiemail.usu.edu>
2020-04-25 21:17:07 -0600

    Screen doesn't flinch when object is chosen

--- script/index.js
+++ script/index.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
   $('.snap-row').scrollLeft('100px');
+
+  $('.snap-child').click(function() {
+    let pos = $(window).scrollTop();
+
+    if($(this).hasClass('snap-child-active'))
+      $(this).removeClass('snap-child-active');
+    else
+      $(this).addClass('snap-child-active');
+
+    $(this).siblings().removeClass('snap-child-active');
+    // $(this).parent().scrollLeft(`${$(this).parent().scrollLeft() + 2}px`);
+    $(this).get(0).scrollIntoView()
+    $(window).scrollTop(pos);
+  });
 });
